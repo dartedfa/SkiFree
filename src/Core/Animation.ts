1a92ac13189ef95d70526ca818a0591caaf8c984
Gio Gelashvili <giorgi.gelashvili128@gmail.com>
2023-10-17 22:09:35 +0400

    Refactored Animation class to controll frame changes.

--- src/Core/Animation.ts
+++ src/Core/Animation.ts
@@ -3,7 +3,7 @@
  * to the start when the full animation sequence has been played or they can not loop and just finish on the last image.
  * They can also have a callback set to fire when the animation sequence is complete.
  */
-import { IMAGE_NAMES } from "../Constants";
+import { ANIMATION_FRAME_SPEED_MS, IMAGE_NAMES } from "../Constants";
 
 export class Animation {
     /**
@@ -17,14 +17,45 @@ export class Animation {
     private readonly looping: boolean;
 
     /**
+     * The current frame of the current animation the rhino is on.
+     */
+    currentAnimationFrame: number = 0;
+
+    /**
+     * The time in ms of the last frame change. Used to provide a consistent framerate.
+     */
+    animationFrameTime: number = Date.now();
+
+    /**
      * Function to call when the animation is complete
      */
     private readonly callback?: Function;
 
-    constructor(images: IMAGE_NAMES[], looping: boolean, callback?: Function) {
+    /**
+     * Function to be called when the current frame changes
+     */
+    private readonly getCurrentImage: Function;
+
+    constructor(images: IMAGE_NAMES[], looping: boolean, getCurrentImage: Function,  callback?: Function) {
         this.images = images;
         this.looping = looping;
         this.callback = callback;
+        this.getCurrentImage = getCurrentImage
+    }
+
+    animate(gameTime: number) {
+        if (gameTime - this.animationFrameTime > ANIMATION_FRAME_SPEED_MS) {
+            this.nextAnimationFrame(gameTime);
+        }
+    }
+
+    finishAnimation() {
+        this.currentAnimationFrame = 0;
+        const animationCallback = this.getCallback();
+
+        if (animationCallback) {
+            animationCallback.apply(null);
+        }
     }
 
     getImages(): IMAGE_NAMES[] {
@@ -37,5 +68,23 @@ export class Animation {
 
     getCallback(): Function | undefined {
         return this.callback;
+    }
+
+    nextAnimationFrame(gameTime: number) {
+
+        const animationImages = this.getImages();
+
+        this.animationFrameTime = gameTime;
+        this.currentAnimationFrame++;
+        if (this.currentAnimationFrame >= animationImages.length) {
+            if (!this.getLooping()) {
+                this.finishAnimation();
+                return;
+            }
+
+            this.currentAnimationFrame = 0;
+        }
+
+        this.getCurrentImage(animationImages[this.currentAnimationFrame])
     }
 }
